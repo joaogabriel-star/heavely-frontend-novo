c391428e5dbe9a359b1142b9bceeabe17315074e
joaogabriel-star <joaogabrielsouza706@gmail.com>
2026-05-26 22:36:53 -0300

    Configurar URL de produção da API

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,7 @@ import axios from 'axios';
 
 // 1. Configuração do Axios
 const api = axios.create({
-  baseURL: 'http://localhost:5074/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5074/api'
 });
 
 // Interceptor para o Token
