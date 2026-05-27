1ba628a1d6fa8a6c1a3b271dce5e3d1beec79e35
joaogabriel-star <joaogabrielsouza706@gmail.com>
2026-05-26 23:57:50 -0300

    Forcando o Vercel a ler o codigo atualizado

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 1. Configuração do Axios
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5074/api'
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Interceptor para o Token
