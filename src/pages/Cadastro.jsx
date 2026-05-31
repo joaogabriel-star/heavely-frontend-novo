eeb54b41cd67381b4ac04380aea50c8fdd7a0dcf
joaogabriel-star <joaogabrielsouza706@gmail.com>
2026-05-30 22:36:25 -0300

    Arrumando o botão de criar conta

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Cadastro.css';
-import { api } from '../services/api'; // Importamos a instância do Axios
+import { authService } from '../services/api'; // Importamos a instância do Axios
 
 export const Cadastro = () => {
   const navigate = useNavigate();
